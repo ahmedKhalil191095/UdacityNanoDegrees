fb77a9e80f8a2a9477f255359a9f1f7ce02456d8
Ahmed Khalil <Ahmed.fathalla@thirdwayv.com>
2024-11-12 14:51:43 +0200

    Update aws-eb.sh file.

--- udagram/udagram-api/bin/aws-eb.sh
+++ udagram/udagram-api/bin/aws-eb.sh
@@ -3,4 +3,5 @@ sudo apt-get -y -qq update
 #sudo apt-get install python3-pip python-dev build-essential
 sudo apt-get update && sudo apt-get install -y python3-pip
 sudo pip3 install --upgrade setuptools
+python -m pip install 'botocore>=1.35.0,<1.36.0'
 sudo pip3 install awsebcli --upgrade
