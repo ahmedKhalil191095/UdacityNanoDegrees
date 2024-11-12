690e406cba987333e840b2df832e7f4ed872ecc4
Ahmed Khalil <Ahmed.fathalla@thirdwayv.com>
2024-11-12 10:48:07 +0200

    Updata Bucket name.

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -7,15 +7,17 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
+  username: "postgres",
+  password: "postgres",
+  database: "postgres",
+  host: "postgres.c8swkvtplevs.us-east-1.rds.amazonaws.com",
+  aws_region: "",
+  aws_profile: "",
+  aws_media_bucket: "",
+  url: "http://localhost:8080",
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: "process.env.JWT_SECRET",
   },
+  accessKeyId: "",
+  secretAccessKey: ""
 };
--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -5,8 +5,14 @@ export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
-  host: config.host,
+  host: "postgres.c8swkvtplevs.us-east-1.rds.amazonaws.com",
 
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Use only for testing; should be true in production
+    }
+  },
   storage: ":memory:",
 });
