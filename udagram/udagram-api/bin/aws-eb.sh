193acfa326644b26ce081ceca36c59ae6462c154
Ahmed Khalil <Ahmed.fathalla@thirdwayv.com>
2024-11-12 13:55:39 +0200

    Update aws-eb.sh

--- udagram/udagram-api/bin/aws-eb.sh
+++ udagram/udagram-api/bin/aws-eb.sh
@@ -1,5 +1,6 @@
 # Installing AWS EB CLI
 sudo apt-get -y -qq update
-sudo apt-get install python3-pip python-dev build-essential
-sudo pip install --upgrade setuptools
-sudo pip install awsebcli --upgrade
+#sudo apt-get install python3-pip python-dev build-essential
+sudo apt-get update && sudo apt-get install -y python3-pip
+sudo pip3 install --upgrade setuptools
+sudo pip3 install awsebcli --upgrade
